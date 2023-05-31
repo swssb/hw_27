7635f04105d4a61e2e83a4bb102180790daa852c
vladarik <swssb@bk.ru>
2023-05-31 21:36:56 +0700

    models

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,6 @@
+import json
+
+from django.db import IntegrityError
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.utils.decorators import method_decorator
@@ -30,6 +33,15 @@ class CategorieView(View):
             )
         return JsonResponse(response, safe=False, status=200)
 
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            categorie = Categorie.objects.create(name=data.get("name"))
+        except IntegrityError:
+            return JsonResponse({"error": "this column does not exist"})
+        return JsonResponse({"id": categorie.id, "name": categorie.name}, status=200)
+
+
 class AdListView(ListView):
     model = Ad
     def get(self, request, *args, **kwargs):
@@ -45,6 +57,22 @@ class AdListView(ListView):
                 }
             )
         return JsonResponse(response, safe=False, status=200)
+
+    def post(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+        data = json.loads(request.body)
+        ad = Ad.objects.create(name=data.get("name"), author=data.get("author"), price=data.get("price"),
+                               description=data.get('description'), address=data.get('address'),
+                               is_published=data.get("is_published"))
+        return JsonResponse({
+            "id": ad.id,
+            "name": ad.name,
+            "author": ad.author,
+            "price": ad.price,
+            "description": ad.description,
+            "address": ad.address,
+            "is_published": ad.is_published
+        }, status=200)
 
 class CategorieEntityView(View):
     def get(self, request, id):
