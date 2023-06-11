01dc6cb9ecef4d2f6437a02ed63b0f2a0a1bc177
vladarik <swssb@bk.ru>
2023-06-11 12:38:38 +0700

    hw29: step 0

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from users.views import UserListView, UserDetailView, UserCreateView, UserUpdateView, UserDeleteView
+from users.views import UserListView, UserDetailView, UserUpdateView, UserDeleteView, \
+    UserGenericCreateView
 
 urlpatterns = [
     path("user/", UserListView.as_view()),
     path("user/<int:pk>/", UserDetailView.as_view()),
-    path("user/create/", UserCreateView.as_view()),
+    path("user/create/", UserGenericCreateView.as_view()),
     path("user/<int:pk>/update/", UserUpdateView.as_view()),
     path("user/<int:pk>/delete/", UserDeleteView.as_view())
 ]
