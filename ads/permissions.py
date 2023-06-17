edf049e2d0b8c9d378e2b0eb5fefbe6d1a2e2a02
vladarik <swssb@bk.ru>
2023-06-17 11:35:32 +0700

    done 30

--- ads/permissions.py
+++ ads/permissions.py
@@ -1,11 +1,15 @@
 from rest_framework import permissions
 
+from ads.models import Ad
 from users.models import User
 
 
-class AdChangePermission(permissions.BasePermission):
+class AdChangePermission(permissions.BasePermission):#TODO
     message = "Changing ad can only admins or user who create the ad"
     def has_permission(self, request, view):
-        if request.user.role in [User.ADMIN, User.MODERATOR] or request.body.author == request.user.id:
+        if request.user.role in [User.ADMIN, User.MODERATOR]:
+            return True
+        entity = Ad.objects.get(pk=view.kwargs["pk"])
+        if entity.author_id == request.user.id:
             return True
         return False
--- ads/urls.py
+++ ads/urls.py
@@ -8,14 +8,17 @@ urlpatterns = [
     path("cat/", CategoryListView.as_view()),
     path("cat/create/", CategoryCreateView.as_view()),
     path("cat/<int:pk>/", CategoryDetailView.as_view()),
+    path("cat/<int:pk>/update/", CategoryUpdateView.as_view()),
+    path("cat/<int:pk>/delete/", CategoryDeleteView.as_view()),
+
+
     path("ad/", AdSearchView.as_view()),
     path("ad/create/", AdCreateView.as_view()),
     path("ad/<int:pk>/", AdRetrieveView.as_view()),
-    path("cat/<int:pk>/update/", CategoryUpdateView.as_view()),
     path("ad/<int:pk>/update/", AdUpdateView.as_view()),
-    path("cat/<int:pk>/delete/", CategoryDeleteView.as_view()),
     path("ad/<int:pk>/delete/", AdDeleteView.as_view()),
     path("ad/<int:pk>/upload_image/", AdImageView.as_view()),
+
     path("selection/create/", SelectionCreateView.as_view()),
     path("selection/", SelectionListView.as_view()),
     path("selection/<int:pk>/", SelectionRetrieveView.as_view()),
